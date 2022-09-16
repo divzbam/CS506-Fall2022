c7d802d82ecb06bfa6ceb8605ae16c73ddec8f5c
divzbam <dbamalwa@bu.edu>
2022-09-16 12:32:35 -0400

    Add existing file

---
+++ labs/01-lab/infrastructure/coolestlake.py
@@ -0,0 +1,12 @@
+def draw_lake():
+    print("lake not found")
+    print("🌳 ------- 🌳")
+    print(" /        \ ")
+    print("(          )")
+    print("|  coolest |")
+    print("|   lake   |")
+    print("|   ever   |")
+    print("(          )")
+    print(" \        / ")
+    print("🌳 ------- 🌳")
+    return
